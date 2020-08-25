30e00c674b4847912f550815e7318567112d6932
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-25 11:38:31 +0530

    getting started with componebnt state

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Navbar extends Component {
+    // default props
   static defaultProps = {
     title: ' Github Finder',
     icon: 'fab fa-github',
   };
 
-  static PropTypes = {
+//   propTypes are basically type checking
+  static propTypes = {
     title: PropTypes.string.isRequired,
     icon: PropTypes.string.isRequired,
   };
