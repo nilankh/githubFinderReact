0bd5343c84bf0733dd7c314eee5f1efc2d69c3f6
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-25 10:04:05 +0530

    done with components props and propTypes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-       <h1>Hello</h1> 
+      <div className="App">
+        <Navbar />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
