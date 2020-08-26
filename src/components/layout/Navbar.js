c2aa0357fa5bcf4318cd642133e2e1e8009adf4d
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-08-26 07:58:01 +0530

    stateless functionalcomponets converted class based component to functional based component reason is that they no longer need of state

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,29 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Navbar extends Component {
-    // default props
-  static defaultProps = {
-    title: ' Github Finder',
-    icon: 'fab fa-github',
-  };
+const Navbar = (props) => {
+  return (
+    <nav className="navbar bg-primary ">
+      <h1>
+        <i className={props.icon} />
+        {props.title}
+      </h1>
+    </nav>
+  );
+};
+// default props
+Navbar.defaultProps = {
+  title: ' Github Finder',
+  icon: 'fab fa-github',
+};
 
 //   propTypes are basically type checking
-  static propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-  };
-
-  render() {
-    return (
-      <nav className="navbar bg-primary ">
-        <h1>
-          <i className={this.props.icon} />
-          {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
-
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
 export default Navbar;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,27 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  
-  render() {
-    const { login, avatar_url, html_url } = this.props.user;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
+// const UserItem = (props) => {
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  // const { login, avatar_url, html_url } = props.user;
+  // u can destructure in line 5 also which i have done same thing as line6
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
 
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
