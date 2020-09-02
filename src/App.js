f8a03b358c0f85f08dd28e387cca198e5fde4180
NILANK NIKHIL <nilanknikhil@gmail.com>
2020-09-02 09:20:24 +0530

    Moving Repos state to Context

--- src/App.js
+++ src/App.js
@@ -5,79 +5,16 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import User from './components/users/User';
 import Search from './components/users/Search';
-import axios from 'axios';
+
 import GithubState from './context/github/GithubState';
 import './App.css';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 
 const App = () => {
-  
-  const [repos, setRepos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [alert, setAlert] = useState(null);
-  // this state can be written like this is functional component with the helps iof hooks so below state and above piece of code represenets same
-  // state = {
-  //   users: [],
-  //   user: {},
-  //   repos: [],
-  //   loading: false,
-  //   alert: null,
-  // };
-
-  // Search Github users(moved to githubstate.js file on 27lecture)
-  // const searchUsers = async (text) => {
-  //   // this.setState({ loading: true });
-  //   setLoading(true);
-  //   const res = await axios.get(
-  //     `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-  //   );
-  //   // console.log(res.data.items);
-  //   setUsers(res.data.items);
-  //   setLoading(false);
-  //   // in hooks these below two lines can be wriiten as above two lines written or v bht jagahn smiliarity h
-  //   // this.setState({
-  //   //   users: res.data.items,
-  //   //   loading: false,
-  //   // });
-  // };
-
-  // // Get single Github user
-  // const getUser = async (username) => {
-  //   // this.setState({ loading: true });
-  //   setLoading(true);
-  //   // console.log(text)
-  //   const res = await axios.get(
-  //     `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-  //   );
-  //   // console.log(res.data.items);
-  //   setUser(res.data);
-  //   setLoading(false);
-  //   // this.setState({
-  //   //   user: res.data,
-  //   //   loading: false,
-  //   // });
-  // };
-
-  // Get users repos
-  const getUserRepos = async (username) => {
-    // this.setState({ loading: true });
-    setLoading(true);
-    // console.log(text)
-    const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
-    // console.log(res.data.items);
-    setRepos(res.data);
-    setLoading(false);
-    // this.setState({
-    //   repos: res.data,
-    //   loading: false,
-    // });
-  };
-
  
-
+  const [alert, setAlert] = useState(null);
+  
   // Set Alert
   const showAlert = (msg, type) => {
     // this.setState({ alert: { msg: msg, type: type } });
@@ -106,20 +43,7 @@ const App = () => {
                 )}
               />
               <Route exact path="/about" component={About} />
-              <Route
-                exact
-                path="/user/:login"
-                render={(props) => (
-                  <User
-                    {...props}
-                    
-                    getUserRepos={getUserRepos}
-                    
-                    repos={repos}
-                    
-                  />
-                )}
-              />
+              <Route exact path="/user/:login" component={User} />
             </Switch>
           </div>
         </div>
